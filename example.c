3186f346d170fad96fc8b196a75630c29aa0a155
AbdOooO <AbdOooO.Sarwat@gmail.com>
2019-07-09 16:26:14 +0200

    adding Make file

---
+++ example.c
@@ -0,0 +1,13 @@
+#include"GCD_LCM.h"
+
+int main(){
+    
+    int arr[]={ 12,16,24};
+    
+    int x = (sizeof(arr)/sizeof(arr[0]));
+    
+    printf("least common divider is : %d \n" ,LeastCommonMultiplier(arr,x));
+    printf("great common divider is : %d \n" ,GreatCommonDivider(arr,x));
+
+    return 0 ;  
+}
